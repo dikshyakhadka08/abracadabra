fc4e1d9c50ac33d22cb34c5b0a075f8b6bf4a98b
Dikshya Khadka <dikshya152207@gmail.com>
2022-01-15 10:17:00 +0545

    git assignment

---
+++ TASK 1.py
@@ -0,0 +1,4 @@
+#TASK 1
+print("Stop! Who would cross the Bridge of Death Must answer me these questions three, 'ere the other side he see.")
+name = input("What is your name? ")
+print("Arthur My liege! You may pass!"if name.lower()=="arthur" else  )
